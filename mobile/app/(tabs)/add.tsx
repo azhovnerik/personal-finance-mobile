8afe4496565f54d5cfda903d48d6ba7d6714ff70
andrejandrej <azh@anadeainc.com>
2026-08-11 14:56:59 +0300

    fix(transactions): open create modal from deep links

--- mobile/app/(tabs)/add.tsx
+++ mobile/app/(tabs)/add.tsx
@@ -1,9 +1,3 @@
-import { useRouter } from "expo-router";
-
-import { CreateTransactionModal } from "../../src/features/transactions/create/CreateTransactionModal";
-
-export default function AddTransactionScreen() {
-  const router = useRouter();
-
-  return <CreateTransactionModal visible onClose={() => router.replace("/(tabs)")} />;
+export default function AddTabPlaceholder() {
+  return null;
 }
